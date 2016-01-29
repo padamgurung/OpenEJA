93d1f68a30caf28a625784d6c24af7f1fbfc5e91
padam raj gurung <padam.gurung@ntc.net.np>
2016-01-30 01:41:25 +0545

    ADD, UPDATE, REMOVE for ORM on BO functionality added

--- App/src/OpenEJA/App/Program.java
+++ App/src/OpenEJA/App/Program.java
@@ -1,59 +1,24 @@
 package OpenEJA.App;
 
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 import OpenEJA.BO.User;
-import OpenEJA.DAL.DBAdapter;
-import OpenEJA.DAL.DBFactoryAdapter;
+
 
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		//DAL Testing	
-		
-		/*DBAdapter dbAdapter = DBFactoryAdapter.getDBAdapter();	
-		HashMap<String, String> newData = new HashMap<String, String>();
-		newData.put("name", "Padam Raj Gurung");
-		newData.put("email", "padam.gurung@outlook.com");
-		newData.put("mobile_number", "9851126422");
-		dbAdapter.insert("user", newData);
-		HashMap<String, String> modifiedData = new HashMap<String, String>();
-		modifiedData.put("name", "Mr. Padam Raj Gurung");
-		HashMap<String, String> whereCondition = new HashMap<String, String>();
-		whereCondition.put("mobile_number", "9851126422");
-		dbAdapter.update("user", modifiedData, whereCondition);
-		HashMap<String, String> whereDeleteCondition = new HashMap<String, String>();
-		whereDeleteCondition.put("id", "3");
-		dbAdapter.delete("user", whereDeleteCondition);
-		
-		List<String> columns = new ArrayList<String>();
-	
-		ResultSet myRs = dbAdapter.retrieve("user", whereCondition, columns, "id ASC", 0, 5);
-		try {
-			while (myRs.next()) {
-				System.out.println(myRs.getString("id") + ", " + myRs.getString("name") + ", "
-						+ myRs.getString("email") + ", " + myRs.getString("mobile_number"));
-			System.out.println("Main Application Testing Done!!");
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		
+			
 		//BO and ORM Testing
 		
 		User user = new User("User");
-		user.setEmail("padam.gurung@outlook.com");
-		user.setName("Padam Raj Gurung");
-		user.setMobileNumber("9851126422");		
-		user.add();
+		user.id = 37;
+		user.email = "padamg.urung@outlook.com";
+		user.name = "padam gurung";
+		user.mobileNumber = "9851126422";	
+		//user.add();
+		//user.update();
+		user.remove();
 		
 		
 		System.out.println("ORM Testing Done!!");
