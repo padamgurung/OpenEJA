e011a14cbdfde9b2d09ebf3043c1319707cd4e0a
padam raj gurung <padam.gurung@ntc.net.np>
2016-01-29 08:13:58 +0545

    Annotation added to User BO

--- App/src/OpenEJA/App/Program.java
+++ App/src/OpenEJA/App/Program.java
@@ -21,12 +21,12 @@ public class Program {
 		HashMap<String, String> newData = new HashMap<String, String>();
 		newData.put("name", "Padam Raj Gurung");
 		newData.put("email", "padam.gurung@outlook.com");
-		newData.put("mobileNumber", "9851126422");
+		newData.put("mobile_number", "9851126422");
 		dbAdapter.insert("user", newData);
 		HashMap<String, String> modifiedData = new HashMap<String, String>();
 		modifiedData.put("name", "Mr. Padam Raj Gurung");
 		HashMap<String, String> whereCondition = new HashMap<String, String>();
-		whereCondition.put("mobileNumber", "9851126422");
+		whereCondition.put("mobile_number", "9851126422");
 		dbAdapter.update("user", modifiedData, whereCondition);
 		HashMap<String, String> whereDeleteCondition = new HashMap<String, String>();
 		whereDeleteCondition.put("id", "3");
@@ -38,7 +38,7 @@ public class Program {
 		try {
 			while (myRs.next()) {
 				System.out.println(myRs.getString("id") + ", " + myRs.getString("name") + ", "
-						+ myRs.getString("email") + ", " + myRs.getString("mobileNumber"));
+						+ myRs.getString("email") + ", " + myRs.getString("mobile_number"));
 			System.out.println("Main Application Testing Done!!");
 			}
 		} catch (SQLException e) {
@@ -50,8 +50,7 @@ public class Program {
 		User user = new User("User");
 		user.setEmail("padam.gurung@outlook.com");
 		user.setName("Padam Raj Gurung");
-		user.setMobileNumber("9851126422");
-		
+		user.setMobileNumber("9851126422");		
 		user.add();
 		
 		
